311a55c917db6d29f2e5832f29fc2e1912af960c
sami <you@example.com>
2020-08-18 16:31:37 +0200

    added

---
+++ Documents/gameProject/index.js
@@ -0,0 +1,14 @@
+var gameWidth = 400;
+var gameHeight = 400;
+
+var score = 0;
+var lives = 5;
+
+var win = 0;
+var lose = 0;
+
+var ballDiameter = 10;
+var ball = {x: gameWidth/2, y: gameHeight * 2/3},
+            w: ballDiameter , h: ballDiameter};
+
+var dx = -2
