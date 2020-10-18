a9941cd79cb8c55bc2e170064089c51dde9120f4
sami <you@example.com>
2020-10-18 21:51:43 +0200

    firstProject

--- basic-react-website/src/App.jsx
+++ basic-react-website/src/App.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Header from './components/Header/Header';
 
+
+
+
 import './App.scss';
 
 function App() {
